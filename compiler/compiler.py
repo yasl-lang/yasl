6f9f7f03d64f8ec43e3ef5e1e2d223cd459f153c
Thiabaud Engelbrecht <thiabaud.engelbrecht@gmail.com>
2018-02-20 23:56:50 -0800

    fixed scoping issues

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -11,6 +11,7 @@ BINRESERVED = {
         "-":   [SUB],
         "/":   [DIV],
         "%":   [MOD],
+        "||":  [CONCAT],
         "<":   [GE, NOT],
         "<=":  [GT, NOT],
         ">":   [GT],
