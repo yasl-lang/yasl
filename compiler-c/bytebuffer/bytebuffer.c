2798e22a1422b1025b38e7d26364dbb3928711f3
Thiabaud Engelbrecht <thiabaud.engelbrecht@gmail.com>
2018-05-03 15:36:10 -0700

    fixed resizing bug

--- compiler-c/bytebuffer/bytebuffer.c
+++ compiler-c/bytebuffer/bytebuffer.c
@@ -14,33 +14,35 @@ void bb_del(ByteBuffer *bb) {
 }
 
 void bb_add_byte(ByteBuffer  *bb, char byte) {
-    if (bb->size <= bb->count) bb->bytes = realloc(bb->bytes, bb->count*2);
+    if (bb->size <= bb->count) bb->bytes = realloc(bb->bytes, bb->size = bb->count*2);
     bb->bytes[bb->count++] = byte;
 };
 
 
 void bb_append(ByteBuffer *bb, char *bytes, int64_t bytes_len) {
-    if (bb->size < bb->count + bytes_len) bb->bytes = realloc(bb->bytes, (bb->count+bytes_len)*2);
+    if (bb->size < bb->count + bytes_len) bb->bytes = realloc(bb->bytes, bb->size = (bb->count+bytes_len)*2);
     memcpy(bb->bytes + bb->count, bytes, bytes_len);
     //printf("copied: %s\n", bb->bytes + bb->count);
     bb->count += bytes_len;
 };
 
 void bb_floatbytes8(ByteBuffer *bb, double value) {
-    if (bb->size < bb->count + sizeof(double)) bb->bytes = realloc(bb->bytes, (bb->count+sizeof(double))*2);
+    if (bb->size < bb->count + sizeof(double)) bb->bytes = realloc(bb->bytes, bb->size = (bb->count+sizeof(double))*2);
     memcpy(bb->bytes + bb->count, &value, sizeof(double));
     bb->count += sizeof(double);
 }
 
 void bb_intbytes8(ByteBuffer *bb, int64_t value) {
-    if (bb->size < bb->count + sizeof(int64_t)) bb->bytes = realloc(bb->bytes, (bb->count+sizeof(int64_t))*2);
+    if (bb->size < bb->count + sizeof(int64_t)) bb->bytes = realloc(bb->bytes, bb->size = (bb->count+sizeof(int64_t))*2);
     memcpy(bb->bytes + bb->count, &value, sizeof(int64_t));
     bb->count += sizeof(int64_t);
+    //printf("bb_intbytes8; size: %d, count: %d\n", bb->size, bb->count);
 }
 
 
 void bb_rewrite_intbytes8(ByteBuffer *bb, int64_t index, int64_t value) {
     if (bb->size < index + sizeof(int64_t)) {
+        printf("size: %d, index: %d, sizeof(int64_t): %d, index + sizeof(int64_t): %d\n", bb->size, index, sizeof(int64_t), index+ sizeof(int64_t));
         puts("Bad rewrite_intbytes8: outside range");
         exit(EXIT_FAILURE);
     }
