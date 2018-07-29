74cc3a9582ba9e96fe335c770ad8ecd16624a510
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2018-07-29 11:52:36 -0700

    added tests for if, if - else, if - elseif - else, while

--- test/compilertest.c
+++ test/compilertest.c
@@ -476,6 +476,84 @@ static void test_or() {
     ASSERT_GEN_BC_EQ(expected,"true || false;");
 }
 
+/// Control Flow
+////////////////////////////////////////////////////////////////////////////////
+
+static void test_if() {
+    unsigned char expected[] = {
+            0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_T,
+            BRF_8,
+            0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_T,
+            POP,
+            HALT
+    };
+    ASSERT_GEN_BC_EQ(expected,"if true { true; };");
+}
+
+static void test_ifelse() {
+    unsigned char expected[] = {
+            0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_T,
+            BRF_8,
+            0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_T,
+            POP,
+            BR_8,
+            0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_F,
+            POP,
+            HALT
+    };
+    ASSERT_GEN_BC_EQ(expected,"if true { true; } else { false; };");
+}
+
+static void test_ifelseelseif() {
+    unsigned char expected[] = {
+            0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_T,
+            BRF_8,
+            0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_T,
+            POP,
+            BR_8,
+            0x17, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_F,
+            BRF_8,
+            0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_F,
+            POP,
+            BR_8,
+            0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            NCONST,
+            POP,
+            HALT
+    };
+    ASSERT_GEN_BC_EQ(expected,"if true { true; } elseif false { false; } else { undef; };");
+}
+
+static void test_while() {
+    unsigned char expected[] = {
+            0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_T,
+            BRF_8,
+            0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            BCONST_T,
+            POP,
+            GOTO,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            HALT
+    };
+    ASSERT_GEN_BC_EQ(expected,"while true { true; };");
+}
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
 int compilertest() {
@@ -511,6 +589,11 @@ int compilertest() {
     test_concat();
     test_and();
     test_or();
+    // Control Flow
+    test_if();
+    test_ifelse();
+    test_ifelseelseif();
+    test_while();
 
     return __YASL_TESTS_FAILED__;
 }
