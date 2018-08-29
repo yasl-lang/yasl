cf2f1497728fd859559f10a060918180f96a66c2
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2018-08-28 22:00:05 -0700

    further refinements of the test suite

--- test/compilertest.c
+++ test/compilertest.c
@@ -6,33 +6,13 @@
 #include "unoptest.h"
 #include "literaltest.h"
 #include "iftest.h"
+#include "whiletest.h"
 
 #define RUN(test) __YASL_TESTS_FAILED__ |= test()
 
 SETUP_YATS();
 
 // NOTE: these tests depend on the endianess of the system, so they may fail on big endian systems.
-
-/// Control Flow
-////////////////////////////////////////////////////////////////////////////////
-
-static void test_while() {
-    unsigned char expected[] = {
-            0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            BR_8,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            BCONST_T,
-            BRF_8,
-            0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            BCONST_T,
-            POP,
-            BR_8,
-            0xEB, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-            HALT
-    };
-    ASSERT_GEN_BC_EQ(expected,"while true { true; };");
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -41,8 +21,7 @@ int compilertest() {
     RUN(unoptest);
     RUN(binoptest);
     RUN(iftest);
-    // Control Flow
-    test_while();
+    RUN(whiletest);
 
     return __YASL_TESTS_FAILED__;
 }
