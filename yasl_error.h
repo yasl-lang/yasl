a2eb208a79d3e5541596914b909ec6c5c19aa57c
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2018-09-25 19:45:16 -0700

    added interface for C API

---
+++ yasl_error.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+ * Definition of all YASL error codes.
+ */
+
+ enum YASL_Error {
+     YASL_SUCCESS,       // Successful execution.
+     YASL_SYNTAX_ERROR,  // Syntax error during compilation.
+ };
