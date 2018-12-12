7b18c7ef0560f2c143b10b1be2fcef72457ce613
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2018-12-11 22:51:01 -0800

    update to disallow GNU extension with __VA_ARGS__

--- yasl_include.h
+++ yasl_include.h
@@ -4,8 +4,8 @@
 
 #define MSG_SYNTAX_ERROR "SyntaxError: "
 
-#define YASL_PRINT_ERROR(fmt, ...) printf(K_RED fmt K_END, ##__VA_ARGS__)
-#define YASL_PRINT_ERROR_SYNTAX(fmt, ...) YASL_PRINT_ERROR(MSG_SYNTAX_ERROR fmt, ##__VA_ARGS__)
+#define YASL_PRINT_ERROR(fmt, ...) printf(K_RED fmt K_END, __VA_ARGS__)
+#define YASL_PRINT_ERROR_SYNTAX(fmt, ...) YASL_PRINT_ERROR(MSG_SYNTAX_ERROR fmt, __VA_ARGS__)
 
 #define YASL_PRINT_ERROR_UNDECLARED_VAR(name, line) YASL_PRINT_ERROR_SYNTAX("Undeclared variable %s (line %zd).\n", name, line)
 #define YASL_PRINT_ERROR_CONSTANT(name, line) YASL_PRINT_ERROR_SYNTAX("Cannot assign to constant %s (line %zd).", name, line)
