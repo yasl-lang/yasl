ad48156431cb9e8ff4b98357eb36107e857e857c
Ryan Andersen <ryco117@gmail.com>
2019-01-27 23:48:48 -0800

    Those were some sad mistakes :'(

--- prime/prime.c
+++ prime/prime.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "prime.h"
 
-const int64_t PRIMES_UNDER_200[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199};
+const int64_t PRIMES_UNDER_200[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199};
 
 // returns 1 -- prime, 0 -- not prime, -1 -- undefined
 int is_prime(const int64_t x) {
@@ -10,9 +10,8 @@ int is_prime(const int64_t x) {
     if (x < 4) return 1;
 	int64_t i;
 	for (i = 0; i < (int64_t)(sizeof(PRIMES_UNDER_200)/sizeof(int64_t)); i++) {
-		if ((x % PRIMES_UNDER_200[i]) == 0) {
-            return 0;
-        }
+		if (x == PRIMES_UNDER_200[i]) return 1;
+		if ((x % PRIMES_UNDER_200[i]) == 0) return 0;
 	}
     for (i = 201; i <= floor(sqrt((double)x)); i += 2) {
         if ((x % i) == 0) {
