8aa0f84a28e949fd6428199cc65e2df9d18ddaac
Ryan Andersen <ryco117@gmail.com>
2019-02-08 13:45:41 -0800

    Removing overkill probablistic primality test, 2 small improvements

--- prime/prime.c
+++ prime/prime.c
@@ -10,8 +10,7 @@ int is_prime(const int64_t x) {
     if (x < 4) return 1;
 	int64_t i;
 	for (i = 0; i < (int64_t)(sizeof(PRIMES_UNDER_200)/sizeof(int64_t)); i++) {
-		if (x == PRIMES_UNDER_200[i]) return 1;
-		if ((x % PRIMES_UNDER_200[i]) == 0) return 0;
+		if ((x % PRIMES_UNDER_200[i]) == 0) return (x == PRIMES_UNDER_200[i]);
 	}
     for (i = 201; i <= floor(sqrt((double)x)); i += 2) {
         if ((x % i) == 0) {
@@ -19,46 +18,6 @@ int is_prime(const int64_t x) {
         }
     }
     return 1;
-}
-
-int64_t exp_mod_helper(int64_t base, int64_t exp, const int64_t p) {
-	while (exp < 0) exp += (p-1);
-	exp %= p-1;
-
-	int64_t r = 1;
-	int64_t i = 1;
-	while (i <= exp && i != 0) {
-		if ((exp & i) != 0) r = (r * base) % p;
-		base = (base*base) % p; i <<= 1;
-	}
-	return r;
-}
-// returns 1 -- prime, 0 -- not prime, -1 -- undefined
-int prob_is_prime(const int64_t x, const int64_t count) {
-	// bounds..
-	if (x < 2 || count < 1) return -1;
-
-	// For small numbers, less is more
-	if (x < 1000000) return is_prime(x);
-
-	// Apply Miller-Rabin probablistic primality test
-	int64_t r = 1;
-	int64_t d = (x - 1) >> 1;
-	while (d % 2 == 0) {r++; d >>= 1;}
-	// Ensure 'a' can be squared without overflowing int64_t
-	const int64_t max = 3037000498 < x-3 ? 3037000498 : x-3;
-	for (int64_t i = 0; i < count; i++) {
-		int64_t a = (rand() % max) + 2;
-		int64_t b = exp_mod_helper(a, d, x);
-		if (b == 1 || b == x - 1) continue;
-		int64_t j = 0;
-		for (; j < r-1; j++) {
-			b = (b*b) % x;
-			if (b == x - 1) break;
-		}
-		if (j == r - 1) return 0;
-	}
-	return 1;
 }
 
 int64_t next_prime(int64_t x) {
