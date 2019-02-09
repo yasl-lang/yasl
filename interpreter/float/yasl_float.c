ca2cdb0d0c2d845b06165d9e172906c8dba05563
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2019-02-08 21:37:25 -0800

    cleanup

--- interpreter/float/yasl_float.c
+++ interpreter/float/yasl_float.c
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 
 char *float64_to_str(double d) {
-    int64_t size = snprintf(NULL, 0, "%f", d);
-    char *ptr = malloc(size + 1);
-    sprintf(ptr, "%f", d);
-    while (ptr[size-1] == '0' && ptr[size-2] != '.') {
-        size--;
-    }
-    ptr[size] = '\0';
-    ptr = realloc(ptr, size + 1);
-    return ptr;
+	int64_t size = snprintf(NULL, 0, "%f", d);
+	char *ptr = malloc(size + 1);
+	sprintf(ptr, "%f", d);
+	while (ptr[size - 1] == '0' && ptr[size - 2] != '.') {
+		size--;
+	}
+	ptr[size] = '\0';
+	ptr = realloc(ptr, size + 1);
+	return ptr;
 }
