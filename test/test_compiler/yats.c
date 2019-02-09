cc4b01ae083bab308dfeb1998a8e380a34a67d38
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2019-02-08 21:47:22 -0800

    cleanup

--- test/test_compiler/yats.c
+++ test/test_compiler/yats.c
@@ -18,27 +18,27 @@ Lexer setup_lexer(char *file_contents) {
 
 
 unsigned char *setup_compiler(char *file_contents) {
-    FILE *fptr = fopen("dump.ysl", "w");
-    fwrite(file_contents, 1, strlen(file_contents), fptr);
-    fseek(fptr, 0, SEEK_SET);
-    fclose(fptr);
-    fptr = fopen("dump.ysl", "r");
-    struct Compiler *compiler = compiler_new(fptr);
-    unsigned char *bytecode = compile(compiler);
-    FILE *f = fopen("dump.yb", "wb");
-    if (bytecode == NULL) {
-        fputc(HALT, f);
-    } else {
-        fwrite(bytecode, 1, compiler->code->count + compiler->header->count + 1, f);
-    }
-    fclose(f);
+	FILE *fptr = fopen("dump.ysl", "w");
+	fwrite(file_contents, 1, strlen(file_contents), fptr);
+	fseek(fptr, 0, SEEK_SET);
+	fclose(fptr);
+	fptr = fopen("dump.ysl", "r");
+	struct Compiler *compiler = compiler_new(fptr);
+	unsigned char *bytecode = compile(compiler);
+	FILE *f = fopen("dump.yb", "wb");
+	if (bytecode == NULL) {
+		fputc(HALT, f);
+	} else {
+		fwrite(bytecode, 1, compiler->code->count + compiler->header->count + 1, f);
+	}
+	fclose(f);
 	compiler_cleanup(compiler);
-    return bytecode;
+	return bytecode;
 }
 
 int64_t getsize(FILE *file) {
-    fseek(file, 0, SEEK_END);
-    int64_t size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    return size;
+	fseek(file, 0, SEEK_END);
+	int64_t size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	return size;
 }
