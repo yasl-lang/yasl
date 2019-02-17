27e915cec958af7243a5cf08300c8817aed0df69
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2019-02-17 12:05:56 -0800

    clean up main function

--- main.c
+++ main.c
@@ -11,80 +11,99 @@
 #define VERSION "v0.3.6"
 #define VERSION_PRINTOUT "YASL " VERSION
 
-int main(int argc, char** argv) {
-	// Initialize prng seed
-	srand(time(NULL));
+static int main_error(int argc, char **argv) {
+	puts("ERROR: Too many arguments passed. Type `yasl -h` for help (without the backticks).");
+	return EXIT_FAILURE;
+}
 
-	if (argc > 2) {
-		puts("ERROR: Too many arguments passed. Type `yasl -h` for help (without the backticks).");
-		return EXIT_FAILURE;
-	} else if (argc == 2) {
-		if (!strcmp(argv[1], "-h")) {
-			puts("usage: yasl [option] [file]\n"
-			     "options:\n"
-			     "\t-h: this menu\n"
-			     "\t-V: print current version\n"
-			     "\tfile: name of file containing script"
-			);
-			exit(EXIT_SUCCESS);
-		} else if (!strcmp(argv[1], "-V")) {
-			puts(VERSION_PRINTOUT);
-			exit(EXIT_SUCCESS);
-		}
+static int main_help(int argc, char **argv) {
+	puts("usage: yasl [option] [file]\n"
+	     "options:\n"
+	     "\t-h: this menu\n"
+	     "\t-V: print current version\n"
+	     "\tfile: name of file containing script"
+	);
+	exit(EXIT_SUCCESS);
+}
 
-		struct YASL_State *S = YASL_newstate(argv[1]);
+static int main_version(int argc, char **argv) {
+	puts(VERSION_PRINTOUT);
+	exit(EXIT_SUCCESS);
+}
 
-		if (!S) {
-			puts("ERROR: cannot open file.");
-			exit(EXIT_FAILURE);
-		}
+static int main_file(int argc, char **argv) {
+	if (!strcmp(argv[1], "-h")) {
+		return main_help(argc, argv);
+	} else if (!strcmp(argv[1], "-V")) {
+		return main_version(argc, argv);
+	}
 
-		// Load Standard Libraries
-		YASL_load_math(S);
-		YASL_load_io(S);
+	struct YASL_State *S = YASL_newstate(argv[1]);
 
-		int status = YASL_execute(S);
+	if (!S) {
+		puts("ERROR: cannot open file.");
+		exit(EXIT_FAILURE);
+	}
 
-		YASL_delstate(S);
+	// Load Standard Libraries
+	YASL_load_math(S);
+	YASL_load_io(S);
 
-		return status;
-	} else {
-		int next;
-		size_t size = 8, count = 0;
-		char *buffer = malloc(size);
-		struct YASL_State *S = YASL_newstate_bb(buffer, 0);
-		YASL_load_math(S);
-		YASL_load_io(S);
-		puts(VERSION_PRINTOUT);
-		while (1) {
-			printf("yasl> ");
-			while ((next = getchar()) != '\n') {
-				if (size == count) {
-					size *= 2;
-					buffer = realloc(buffer, size);
-				}
-				buffer[count++] = next;
-			}
+	int status = YASL_execute(S);
+
+	YASL_delstate(S);
+
+	return status;
+}
+
+static int main_REPL(int argc, char **argv) {
+	int next;
+	size_t size = 8, count = 0;
+	char *buffer = malloc(size);
+	struct YASL_State *S = YASL_newstate_bb(buffer, 0);
+	YASL_load_math(S);
+	YASL_load_io(S);
+	puts(VERSION_PRINTOUT);
+	while (1) {
+		printf("yasl> ");
+		while ((next = getchar()) != '\n') {
 			if (size == count) {
 				size *= 2;
 				buffer = realloc(buffer, size);
 			}
-			buffer[count++] = '\n';
-
-			if (count == strlen("quit\n") && !memcmp(buffer, "quit\n", strlen("quit\n"))) {
-				break;
-			}
-			// printf("`%s`", buffer);
-			YASL_resetstate_bb(S, buffer, count); // *S = YASL_newstate_bb(buffer, count);
-
-			count = 0;
-			// Load Standard Libraries
-
-			YASL_execute_REPL(S);
+			buffer[count++] = next;
 		}
-		free(buffer);
-		YASL_delstate(S);
-	}
+		if (size == count) {
+			size *= 2;
+			buffer = realloc(buffer, size);
+		}
+		buffer[count++] = '\n';
 
-	return EXIT_SUCCESS;
+		if (count == strlen("quit\n") && !memcmp(buffer, "quit\n", strlen("quit\n"))) {
+			break;
+		}
+		// printf("`%s`", buffer);
+		YASL_resetstate_bb(S, buffer, count); // *S = YASL_newstate_bb(buffer, count);
+
+		count = 0;
+		// Load Standard Libraries
+
+		YASL_execute_REPL(S);
+	}
+	free(buffer);
+	YASL_delstate(S);
+	return YASL_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+	// Initialize prng seed
+	srand(time(NULL));
+
+	if (argc > 2) {
+		return main_error(argc, argv);
+	} else if (argc == 2) {
+		return main_file(argc, argv);
+	} else {
+		return main_REPL(argc, argv);
+	}
 }
