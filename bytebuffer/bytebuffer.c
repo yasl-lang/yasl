34598ed8b3f0ea2d9de54ab38025adcefd1d4155
gadget114514 <gadget114514@gmail.com>
2019-02-18 07:48:41 +0900

    buffer of bytebuffer out of bounds

--- bytebuffer/bytebuffer.c
+++ bytebuffer/bytebuffer.c
@@ -20,20 +20,20 @@ void bb_add_byte(ByteBuffer  *const bb, const unsigned char byte) {
 
 
 void bb_append(ByteBuffer *const bb, const unsigned char *const bytes, const size_t bytes_len) {
-	if (bb->size < bb->count + bytes_len) bb->bytes = realloc(bb->bytes, bb->size = (bb->count + bytes_len) * 2);
+	if (bb->size <= bb->count + bytes_len) bb->bytes = realloc(bb->bytes, bb->size = (bb->count + bytes_len) * 2);
 	memcpy(bb->bytes + bb->count, bytes, bytes_len);
 	bb->count += bytes_len;
 }
 
 void bb_floatbytes8(ByteBuffer *const bb, const yasl_float value) {
-	if (bb->size < bb->count + sizeof(yasl_float)) bb->bytes = realloc(bb->bytes, bb->size =
+	if (bb->size <= bb->count + sizeof(yasl_float)) bb->bytes = realloc(bb->bytes, bb->size =
 			(bb->count + sizeof(yasl_float)) * 2);
 	memcpy(bb->bytes + bb->count, &value, sizeof(yasl_float));
 	bb->count += sizeof(yasl_float);
 }
 
 void bb_intbytes8(ByteBuffer *const bb, const yasl_int value) {
-	if (bb->size < bb->count + sizeof(yasl_int))
+	if (bb->size <= bb->count + sizeof(yasl_int))
 		bb->bytes = realloc(bb->bytes, bb->size = (bb->count + sizeof(yasl_int)) * 2);
 	memcpy(bb->bytes + bb->count, &value, sizeof(yasl_int));
 	bb->count += sizeof(yasl_int);
@@ -41,7 +41,7 @@ void bb_intbytes8(ByteBuffer *const bb, const yasl_int value) {
 
 
 void bb_rewrite_intbytes8(ByteBuffer *const bb, const size_t index, const yasl_int value) {
-	if (bb->size < index + sizeof(yasl_int)) {
+	if (bb->size <= index + sizeof(yasl_int)) {
 		puts("Bad rewrite_intbytes8: outside range");
 		exit(EXIT_FAILURE);
 	}
