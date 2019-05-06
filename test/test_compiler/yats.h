4ccf61e7f613e61308b80566b3008cb0f88c3c99
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2019-05-06 10:40:59 -0400

    Fixed bug causing incorrect test failures.

--- test/test_compiler/yats.h
+++ test/test_compiler/yats.h
@@ -8,16 +8,16 @@
     static int __YASL_TESTS_FAILED__ = 0
 
 
-#define ASSERT_BC_EQ(left, right) do {\
-    if (sizeof(left) == sizeof(right) && !memcmp(left, right, sizeof(left))) {\
+#define ASSERT_BC_EQ(left, right, sr) do {\
+    if (sizeof(left) == sr && !memcmp(left, right, sizeof(left))) {\
         /*printf(K_GRN "assert passed in %s: line %d" K_END "\n", __func__, __LINE__);*/\
     } else {\
         printf(K_RED "assert failed in %s: line %d (%s)" K_END "\n", __func__, __LINE__, __FILE__);\
         puts("expected: ");\
-        for (size_t i = 0; i < sizeof(left); i++) printf(i < sizeof(right) && (left)[i] == (right)[i] ? K_GRN "%02x " : K_RED "%02x ", (left)[i] & 0xFF);\
+        for (size_t i = 0; i < sizeof(left); i++) printf(i < sr && (left)[i] == (right)[i] ? K_GRN "%02x " : K_RED "%02x ", (left)[i] & 0xFF);\
         printf(K_END "\n");\
         puts("actual: ");\
-        for (size_t i = 0; i < sizeof(right); i++) printf(i < sizeof(left) && (left)[i] == (right)[i] ? K_GRN "%02x " : K_RED "%02x ", (right)[i] & 0xFF);\
+        for (size_t i = 0; i < sr; i++) printf(i < sizeof(left) && (left)[i] == (right)[i] ? K_GRN "%02x " : K_RED "%02x ", (right)[i] & 0xFF);\
         printf(K_END "\n");\
         __YASL_TESTS_FAILED__ = 1;\
     }\
@@ -30,7 +30,7 @@
     int64_t size = getsize(file);\
     unsigned char *actual = (unsigned char *)malloc(size);\
     fread(actual, sizeof(char), size, file);\
-    ASSERT_BC_EQ(expected, actual);\
+    ASSERT_BC_EQ(expected, actual, (size_t)size);\
     fclose(file);\
     free(actual);\
 } while(0)
