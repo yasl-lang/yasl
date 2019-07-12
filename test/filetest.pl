f619f02434ef12a2ed757805a4dd61df81d92fb2
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2019-07-11 21:44:56 -0700

    clean up tests (move expected out to a new file)

--- test/filetest.pl
+++ test/filetest.pl
@@ -5,8 +5,6 @@ my $__MEM_TESTS_FAILED__ = 0;
 
 sub assert_output {
     my ($string, $exp_out, $exp_stat) = @_;
-    my (undef, $filename, $line) = caller;
-
 
     my $RED = "\x1B[31m";
     my $END = "\x1B[0m";
@@ -16,11 +14,11 @@ sub assert_output {
     my $exitcode = !($output eq $exp_out && $status == $exp_stat) || 0;
 
     if ($status != $exp_stat) {
-            print $RED . "exitcode assert failed in $filename (line $line): $status =/= $exp_stat" . $END . "\n";
+            print $RED . "exitcode assert failed in $string: $status =/= $exp_stat" . $END . "\n";
     }
 
     if ($output ne $exp_out) {
-        print $RED . "output assert failed in $filename (line $line): $exp_out =/= $output" . $END . "\n";
+        print $RED . "output assert failed in $string: $exp_out =/= $output" . $END . "\n";
     }
 
     $__MEM_TESTS_FAILED__ ||= $exitcode;
@@ -29,12 +27,11 @@ sub assert_output {
 
 while (defined(my $file = glob 'inputs/*.yasl')) {
     # print "Testing $file for leaks...\n";
-    open my ($filename), $file;
+    local $/;
+    open my ($filename), "$file.out";
     my $line = <$filename>;
     close $filename;
-    $line =~ s/^##//;
-    $line =~ s/\n$//;
-    assert_output($file, eval '"' . $line . '"', 0);
+    assert_output($file, $line, 0);
 }
 
 
