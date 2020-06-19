07427384dac065efec2d46d5c5165bafd788603e
CoffeeTableEspresso <coffeetableespresso@gmail.com>
2020-06-19 15:01:16 -0700

    update test.sh

--- tests.sh
+++ tests.sh
@@ -4,10 +4,26 @@ shopt -s globstar nullglob dotglob
 
 declare -i failed=0;
 
+[[ "$1" == "-m" ]];
+declare MEM=$?;
+
+run_mem_tests () {
+    declare folder="$1";
+    echo "Running memory tests on $folder...";
+    for f in test/$folder/**/*.yasl; do
+        valgrind --error-exitcode=1 --leak-check=full ./yasl $f > /dev/null 2>&1;
+        declare exit_code=$?;
+        if (( exit_code != 0 )); then
+            echo "Memory Error in $f";
+            (( ++failed ));
+        fi;
+    done;
+}
 
 run_tests () {
     declare folder="$1";
     declare expected_exit="$2";
+    echo "Running tests on $folder...";
     declare ext;
     case "$expected_exit" in
         0) ext=".out" ;;
@@ -17,7 +33,7 @@ run_tests () {
         declare expected actual;
         expected=$(<"$f$ext");
         actual=$(./yasl "$f" 2>&1);
-        exit_code=$?;
+        declare exit_code=$?;
         if [[ "$expected" != "$actual" || $exit_code -ne $expected_exit ]]; then
             echo "Failed test for $f. Exited with $exit_code, expected $expected_exit.";
             echo "Expected:";
@@ -27,7 +43,9 @@ run_tests () {
             (( ++failed ));
         fi;
     done;
-
+    if (( MEM == 0 )); then
+        run_mem_tests $1;
+    fi;
 }
 
 run_tests inputs 0
