265e9bc3b4e28466f4fbbd6f2807a933a0abc3a1
CoffeeTableEspresso <coffeetableespresso@gmail.com>
2020-06-19 13:25:34 -0700

    added bash script for tests

---
+++ tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+shopt -s globstar nullglob dotglob
+
+declare -i failed=0;
+
+
+run_tests () {
+    declare folder="$1";
+    declare expected_exit="$2";
+    declare ext;
+    case "$expected_exit" in
+        0) ext=".out" ;;
+        *) ext=".err" ;;
+    esac
+    for f in test/$folder/**/*.yasl; do
+        declare expected actual;
+        expected=$(<"$f$ext");
+        actual=$(./yasl "$f" 2>&1);
+        exit_code=$?;
+        if [[ "$expected" != "$actual" || $exit_code -ne $expected_exit ]]; then
+            echo "Failed test for $f. Exited with $exit_code, expected $expected_exit.";
+            echo "Expected:";
+            echo "$expected"
+            echo "Got:"
+            echo "$actual"
+            (( ++failed ));
+        fi;
+    done;
+
+}
+
+run_tests inputs 0
+run_tests errors/assert 10
+run_tests errors/stackoverflow 11
+run_tests errors/type 5
+
+echo "Failed $failed script tests.";
+
+exit $failed;
