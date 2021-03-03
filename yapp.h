2f01108337639b2765aaf9bb88fb32d1731e7ecf
CoffeeTableEspresso <coffeetableespresso@gmail.com>
2021-03-02 22:54:07 -0500

    attempt 2

---
+++ yapp.h
@@ -0,0 +1,10 @@
+#ifndef YASL_YAPP_H_
+#define YASL_YAPP_H_
+
+/*
+ * This header defines a bunch of useful macros for pre-processor operations, used throughout the YASL implementation.
+ */
+
+
+
+#endif
